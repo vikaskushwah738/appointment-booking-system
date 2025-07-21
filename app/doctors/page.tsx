ad09acda938013e562c235a8dba4beb8b6889fdb
vikaskushwah738 <vikaskushwah738@gmail.com>
2025-07-21 08:35:09 +0530

    adding images

---
+++ app/doctors/page.tsx
@@ -0,0 +1,11 @@
+
+
+const Doctors = () => {
+    return (
+        <main className="pt-16">
+
+        </main>
+    )
+}
+
+export default Doctors
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
+import CTA from "@/components/CTA";
+import Features from "@/components/Features";
+import HereSection from "@/components/HereSection";
+import Prices from "@/components/Price";
+import { Testimonials } from "@/components/Testimonials";
 
 export default function Home() {
   return (
-    <div className="">
-     
-    </div>
+    <main className="bg-background">
+      <HereSection />
+      <Features />
+      <Prices />
+      <Testimonials />
+      <CTA />
+    </main>
   );
 }
